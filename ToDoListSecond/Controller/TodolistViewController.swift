5a2b0b98af5028057cbe60e2ea66a3e640f8dd3f
Hongbo Niu <hongbonew@gmail.com>
2018-03-23 11:23:35 -0400

    Initial Commit

---
+++ ToDoListSecond/Controller/TodolistViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  ToDoListSecond
+//
+//  Created by Hongbo Niu on 2018-03-23.
+//  Copyright © 2018 Hongbo Niu. All rights reserved.
+//
+
+import UIKit
+
+class TodolistViewController: UITableViewController {
+
+    var itemArray = ["buy shoes","buy rice","buy water"]
+    let defaults = UserDefaults.standard
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
+    //MARK: - TableView Datasource Methods
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+        
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    //    MARK: - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+          tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+      tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+
+    //MARK: - Add New Items
+    @IBAction func addItemButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Items", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add", style: .default) { (alertAction) in
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "add new items"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+}
+
---
+++ ToDoListSecond/Model/ItemsModel.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsModel.swift
+//  ToDoListSecond
+//
+//  Created by Hongbo Niu on 2018-03-23.
+//  Copyright © 2018 Hongbo Niu. All rights reserved.
+//
+
+import Foundation
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
